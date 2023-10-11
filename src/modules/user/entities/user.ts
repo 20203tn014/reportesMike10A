dd776e803196764581ae47019426c6a35c512cf0
Rodolfo Campos <20203tn014@utez.edu.mx>
2023-10-10 19:03:11 -0600

    update 10-10-2023

--- src/modules/user/entities/user.ts
+++ src/modules/user/entities/user.ts
@@ -3,9 +3,29 @@ import { Entity, TStatus } from "../../../kernel/types";
 export type TUser = Entity<number> & {
     username: string;
     password?: string;
-    token?: string;
-    lastSignIn?: string;
-    createdAt?: string;
+    userDetails?: JSON;
+    type?: number;
     status?: TStatus;
     person?:any;
+};
+
+export type UserAreas = Entity<number> & {
+    user: TUser;
+    area: Area;
+    createdAt?: string;
+    status?: TStatus;
+};
+
+export type Area = Entity<number> & {
+    name: string;
+    academicDivision?: AcademicDivision;
+    createdAt?: string;
+    status?: TStatus;
+};
+
+export type AcademicDivision = Entity<number> & {
+    name: string;
+    academicDivision?: string;
+    createdAt?: string;
+    status?: TStatus;
 };
